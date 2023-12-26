e09bbf30393b67ba765bb02bc81aca31bf796922
YuminJo <nationmilo@gmail.com>
2023-12-26 23:25:44 +0900

    Added isPaid Post

--- src/main/java/com/ll/medium/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/medium/domain/post/post/entity/Post.java
@@ -1,16 +1,27 @@
 package com.ll.medium.domain.post.post.entity;
 
-import com.ll.medium.domain.member.member.entity.Member;
-import jakarta.persistence.*;
-import lombok.*;
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import com.ll.medium.domain.member.member.entity.Member;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-import static lombok.AccessLevel.PROTECTED;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor(access = PROTECTED)
@@ -32,4 +43,5 @@ public class Post {
     private String title;
     private String body;
     private boolean isPublished;
+    private boolean isPaid;
 }
--- src/main/java/com/ll/medium/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/medium/domain/post/post/service/PostService.java
@@ -1,43 +1,46 @@
 package com.ll.medium.domain.post.post.service;
 
-import com.ll.medium.domain.member.member.entity.Member;
-import com.ll.medium.domain.post.post.entity.Post;
-import com.ll.medium.domain.post.post.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.post.post.entity.Post;
+import com.ll.medium.domain.post.post.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostService {
-    private final PostRepository postRepository;
+	private final PostRepository postRepository;
 
-    @Transactional
-    public void write(Member author, String title, String body, boolean isPublished) {
-        Post post = Post.builder()
-                .author(author)
-                .title(title)
-                .body(body)
-                .isPublished(isPublished)
-                .build();
+	@Transactional
+	public void write(Member author, String title, String body, boolean isPublished, boolean isPaid) {
+		Post post = Post.builder()
+			.author(author)
+			.title(title)
+			.body(body)
+			.isPublished(isPublished)
+			.isPaid(isPaid)
+			.build();
 
-        postRepository.save(post);
-    }
+		postRepository.save(post);
+	}
 
-    public Object findTop30ByIsPublishedOrderByIdDesc(boolean isPublished) {
-        return postRepository.findTop30ByIsPublishedOrderByIdDesc(isPublished);
-    }
+	public Object findTop30ByIsPublishedOrderByIdDesc(boolean isPublished) {
+		return postRepository.findTop30ByIsPublishedOrderByIdDesc(isPublished);
+	}
 
-    public Optional<Post> findById(long id) {
-        return postRepository.findById(id);
-    }
+	public Optional<Post> findById(long id) {
+		return postRepository.findById(id);
+	}
 
-    public Page<Post> search(String kw, Pageable pageable) {
-        return postRepository.findByTitleContainingIgnoreCaseOrBodyContainingIgnoreCase(kw, kw, pageable);
-    }
+	public Page<Post> search(String kw, Pageable pageable) {
+		return postRepository.findByTitleContainingIgnoreCaseOrBodyContainingIgnoreCase(kw, kw, pageable);
+	}
 }
